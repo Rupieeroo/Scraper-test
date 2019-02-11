3802546bf3e504e3b43f3fc97bac5de7462a6a29
Rupert Adams <RupertNCAdams@aol.com>
2019-02-11 17:52:34 +0000

    experimenting with scraper, added name and price scraping

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,12 @@
+require 'HTTParty'
+require 'Nokogiri'
+
 class Scraper
 
+  doc = HTTParty.get("https://store.nike.com/gb/en_gb/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3?ref=https%253A%252F%252Fwww.google.com%252F")
+  @parse_page ||= Nokogiri::HTML(doc)
   def hello
     return "Hello World"
   end
+
 end
